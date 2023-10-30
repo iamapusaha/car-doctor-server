028be8618a1d822785706e0499e4656df7329e6d
iamapusaha <apusaha392@gmail.com>
2023-10-30 14:19:09 +0600

    setup environment variables and  mongoDB

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 app.use(cors())
 app.use(express.json())
 
+console.log(process.env.DB_USER);
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://apusahamain:s9SM2W9y59BZif27@cluster0.hfqgfmf.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hfqgfmf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
