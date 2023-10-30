ce79248f384234f0cdf7178e2a7491f829b887e4
iamapusaha <apusaha392@gmail.com>
2023-10-30 21:21:11 +0600

    get single service data by id

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
 
-console.log(process.env.DB_USER);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hfqgfmf.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,6 +24,24 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
+        const servicesCollection = client.db("car_doctor").collection("service")
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find();
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const options = {
+                projection: { title: 1, price: 1, img: 1 },
+            };
+            // const movie = await movies.findOne(query, options);
+            const result = await servicesCollection.findOne(query, options)
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
