238f774640dc459352ac7c4045cb8dfae4040526
iamapusaha <apusaha392@gmail.com>
2023-10-31 20:15:14 +0600

    delete booking item by id

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ async function run() {
             res.send(result)
         })
 
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
